26dd212dc2afb7d581de3d48127a177ba269b91b
Mitchell Flint <mflint1@sfsu.edu>
2024-02-27 17:11:03 -0800

    Initial Commit

---
+++ src/main/java/com/mitchell_mod/enchantments/MitchellsEnchantmentsClient.java
@@ -0,0 +1,10 @@
+package com.mitchell_mod.enchantments;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MitchellsEnchantmentsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
